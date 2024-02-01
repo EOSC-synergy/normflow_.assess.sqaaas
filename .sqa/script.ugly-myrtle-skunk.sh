1180850ccdb52b4fe62811c31263fa7f2d5221e9
orviz <orviz@ifca.unican.es>
2024-02-01 13:03:21 +0100

    Add JePL file structure

---
+++ .sqa/script.ugly-myrtle-skunk.sh
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+(
+cd github.com/jkomijani/normflow_ &&
+    git rev-parse --is-inside-work-tree
+)
