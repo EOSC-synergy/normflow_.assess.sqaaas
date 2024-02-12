831e4ae86ab9504156f28758b55cc5a338065275
orviz <orviz@ifca.unican.es>
2024-02-12 12:49:19 +0100

    Add JePL file structure

--- .sqa/script.snazzy-pink-kakapo.sh
+++ .sqa/script.snazzy-pink-kakapo.sh
@@ -1,8 +1,9 @@
 # SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
 #
 # SPDX-License-Identifier: GPL-3.0-only
 
 (
 cd github.com/jkomijani/normflow_ &&
-    get_git_tags.py
+    find_doc_files.py --file_type all
 )
